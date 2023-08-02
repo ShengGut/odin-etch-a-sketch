a8fd5247948df0f1d2874facdbcf5ed7bb89aab2
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-08-01 22:04:56 -0700

    Created button to change grid size

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 const container = document.querySelector("#container");
 
+
 let gridSize = 16;
 
-let cellSize = Math.floor(960 / gridSize);
+function generateGrid () {
+    container.textContent = '';
+    let cellSize = Math.floor(960 / gridSize);
 
-for (let i = 0; i < gridSize; i++) {
-    const row = document.createElement('div');
-    row.classList.add('row');
+    for (let i = 0; i < gridSize; i++) {
+        const row = document.createElement('div');
+        row.classList.add('row');
 
-    for (let j = 0; j < gridSize; j++) {
-        const cell = document.createElement('div');
-        cell.classList.add('cell');
-        cell.style.cssText = `width: ${cellSize}px; height:${cellSize}px; outline-style: solid; outline-color: gray; outline-width: thin;`;
+        for (let j = 0; j < gridSize; j++) {
+            const cell = document.createElement('div');
+            cell.classList.add('cell');
+            cell.style.cssText = `width: ${cellSize}px; height:${cellSize}px; outline-style: solid; outline-color: gray; outline-width: thin;`;
+            cell.addEventListener("mouseover", function (e) {
+                e.target.style.backgroundColor = getRandomColor();
+            });
 
-        cell.addEventListener("mouseover", function (e) {
-            e.target.style.backgroundColor = getRandomColor();
-        });
-
-        row.appendChild(cell);
+            row.appendChild(cell);
+        }
+        container.appendChild(row);
     }
-    container.appendChild(row);
-    
 }
+
+generateGrid();
 
 function getRandomColor() {
     const red = Math.floor(Math.random() * 256);
@@ -30,3 +34,14 @@ function getRandomColor() {
     return `rgb(${red},${green},${blue})`;
 }
 
+
+    const changeGridSizeBtn = document.getElementById('gridButton');
+    changeGridSizeBtn.addEventListener('click', function() {
+        const msg = parseInt(prompt('Enter amount of grid squares (max 96)', '16'));
+        console.log(isNaN(msg));
+        if(!isNaN(msg)) {
+            gridSize = Math.min(100, Math.max(1, parseInt(msg)));
+            generateGrid();
+        } else
+            alert("Input error. Input only numerals");
+    });
