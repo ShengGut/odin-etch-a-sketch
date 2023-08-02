2053935a77b08169f60866785e7abc3821b466d0
ShengGut <kazuna@Kazunas-MacBook-Pro.local>
2023-08-01 21:29:25 -0700

    Created function to randomize color. Added event listener to change color of grid on mouse over

--- script.js
+++ script.js
@@ -2,20 +2,31 @@ const container = document.querySelector("#container");
 
 let gridSize = 16;
 
-let cellSize = 960 / gridSize;
+let cellSize = Math.floor(960 / gridSize);
 
-for (let i = 1; i < gridSize; i++) {
-    let row = document.createElement('div');
+for (let i = 0; i < gridSize; i++) {
+    const row = document.createElement('div');
     row.classList.add('row');
-    row.style.cssText = `width: ${cellSize}px; height:${cellSize}px; outline-style: solid; outline-color: gray; outline-width: thin;`;
 
-    for (let j = 1; j < gridSize; j++) {
-        let col = document.createElement('div');
-        col.classList.add('col');
-        col.style.cssText = `width: ${cellSize}px; height:${cellSize}px; outline-style: solid; outline-color: gray; outline-width: thin;`;
-        container.appendChild(col);
+    for (let j = 0; j < gridSize; j++) {
+        const cell = document.createElement('div');
+        cell.classList.add('cell');
+        cell.style.cssText = `width: ${cellSize}px; height:${cellSize}px; outline-style: solid; outline-color: gray; outline-width: thin;`;
+
+        cell.addEventListener("mouseover", function (e) {
+            e.target.style.backgroundColor = getRandomColor();
+        });
+
+        row.appendChild(cell);
     }
     container.appendChild(row);
     
+}
+
+function getRandomColor() {
+    const red = Math.floor(Math.random() * 256);
+    const green = Math.floor(Math.random() * 256);
+    const blue = Math.floor(Math.random() * 256);
+    return `rgb(${red},${green},${blue})`;
 }
 
